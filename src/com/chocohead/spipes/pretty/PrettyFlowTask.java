491ea27510df775044bd49571a2393535cb548c7
Chocohead <Chocohead@Aroma1997.org>
2019-03-01 03:02:07 +0000

    Stop pipe renderer creating dozens of short lived objects
    The type name might be silly, but it beats a `Triple<Pair<EnumFacing, EnumFacing>, Pair<Point3f, Point3f>, Predicate<IPipe>>`

--- src/com/chocohead/spipes/pretty/PrettyFlowTask.java
+++ src/com/chocohead/spipes/pretty/PrettyFlowTask.java
@@ -1,7 +1,6 @@
 package com.chocohead.spipes.pretty;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.function.Consumer;
@@ -11,7 +10,6 @@ import javax.vecmath.Point3f;
 
 import com.google.common.base.Predicates;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.Triple;
 
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.util.EnumFacing;
@@ -22,6 +20,19 @@ import buildcraft.lib.client.model.ModelUtil;
 import buildcraft.transport.BCTransportSprites;
 
 public class PrettyFlowTask implements Callable<List<Pair<Predicate<IPipe>, Consumer<BufferBuilder>>>> {
+	private static class FaceSideCenterRadius {
+		public final Predicate<IPipe> condition;
+		public final EnumFacing face, side;
+		public final Point3f centre, radius;
+
+		public FaceSideCenterRadius(Predicate<IPipe> condition, EnumFacing face, EnumFacing side, Point3f centre, Point3f radius) {
+			this.condition = condition;
+			this.face = face;
+			this.side = side;
+			this.centre = centre;
+			this.radius = radius;
+		}
+	}
 	private final double[] flow;
 
 	public PrettyFlowTask(double[] flow) {
@@ -32,47 +43,42 @@ public class PrettyFlowTask implements Callable<List<Pair<Predicate<IPipe>, Cons
 	public List<Pair<Predicate<IPipe>, Consumer<BufferBuilder>>> call() throws Exception {
 		List<Pair<Predicate<IPipe>, Consumer<BufferBuilder>>> out = new ArrayList<>();
 
-		List<Predicate<IPipe>> guardConditions = new ArrayList<>();
-		List<Triple<Pair<EnumFacing, EnumFacing>, Point3f, Point3f>> facesSidesCentersRadiuses = new ArrayList<>();
+		List<FaceSideCenterRadius> facesSidesCentersRadiuses = new ArrayList<>();
 		for (EnumFacing face : EnumFacing.VALUES) {
 			double f = flow[face.getIndex()];
 			if (f <= 0) continue; //Skip when no power is flowing
 			float r = (float) (f / 4.1D);
 
-			guardConditions.add(Predicates.alwaysTrue());
-			facesSidesCentersRadiuses.add(
-					Triple.of(
-							Pair.of(face, null),
-							new Point3f(0.5F, 0.5F, 0.5F),
-							new Point3f(r, r, r)
-							)
-					);
+			facesSidesCentersRadiuses.add(new FaceSideCenterRadius(
+					Predicates.alwaysTrue(),
+					face, null,
+					new Point3f(0.5F, 0.5F, 0.5F),
+					new Point3f(r, r, r)
+			));
 			for (EnumFacing side : EnumFacing.VALUES) {
-				guardConditions.add(pipe -> pipe.isConnected(side));
-				facesSidesCentersRadiuses.add(
-						Triple.of(
-								Pair.of(face, side),
-								new Point3f(
-										0.5F + side.getFrontOffsetX() * (0.25F + r / 2),
-										0.5F + side.getFrontOffsetY() * (0.25F + r / 2),
-										0.5F + side.getFrontOffsetZ() * (0.25F + r / 2)
-										),
-								new Point3f(
-										side.getAxis() == EnumFacing.Axis.X ? 0.25F - r / 2 : r,
-												side.getAxis() == EnumFacing.Axis.Y ? 0.25F - r / 2 : r,
-														side.getAxis() == EnumFacing.Axis.Z ? 0.25F - r / 2 : r
-										)
-								)
-						);
+				facesSidesCentersRadiuses.add(new FaceSideCenterRadius(
+						pipe -> pipe.isConnected(side),
+						face, side,
+						new Point3f(
+								0.5F + side.getFrontOffsetX() * (0.25F + r / 2),
+								0.5F + side.getFrontOffsetY() * (0.25F + r / 2),
+								0.5F + side.getFrontOffsetZ() * (0.25F + r / 2)
+						),
+						new Point3f(
+								side.getAxis() == EnumFacing.Axis.X ? 0.25F - r / 2 : r,
+								side.getAxis() == EnumFacing.Axis.Y ? 0.25F - r / 2 : r,
+								side.getAxis() == EnumFacing.Axis.Z ? 0.25F - r / 2 : r
+						)
+				));
 			}
 		}
 
-		Iterator<Predicate<IPipe>> it = guardConditions.listIterator();
-		for (Triple<Pair<EnumFacing, EnumFacing>, Point3f, Point3f> faceSideCenterRadius : facesSidesCentersRadiuses) {
-			EnumFacing face = faceSideCenterRadius.getLeft().getLeft();
-			EnumFacing side = faceSideCenterRadius.getLeft().getRight();
-			Point3f center = faceSideCenterRadius.getMiddle();
-			Point3f radius = faceSideCenterRadius.getRight();
+		for (FaceSideCenterRadius faceSideCenterRadius : facesSidesCentersRadiuses) {
+			EnumFacing face = faceSideCenterRadius.face;
+			EnumFacing side = faceSideCenterRadius.side;
+			Point3f center = faceSideCenterRadius.centre;
+			Point3f radius = faceSideCenterRadius.radius;
+
 			ModelUtil.UvFaceData uvs = null;
 			switch (face.getAxis()) {
 			case X:
@@ -100,6 +106,7 @@ public class PrettyFlowTask implements Callable<List<Pair<Predicate<IPipe>, Cons
 						);
 				break;
 			}
+
 			boolean invert = false;
 			if (side != null) {
 				if (face.getAxis() == EnumFacing.Axis.X && side.getAxis() == EnumFacing.Axis.Y ||
@@ -116,16 +123,16 @@ public class PrettyFlowTask implements Callable<List<Pair<Predicate<IPipe>, Cons
 						1 - uvs.minV
 						);
 			}
+
 			uvs = new ModelUtil.UvFaceData(
 					BCTransportSprites.POWER_FLOW.getInterpU(uvs.minU),
 					BCTransportSprites.POWER_FLOW.getInterpV(uvs.minV),
 					BCTransportSprites.POWER_FLOW.getInterpU(uvs.maxU),
 					BCTransportSprites.POWER_FLOW.getInterpV(uvs.maxV)
 					);
-			assert it.hasNext();
-			out.add(Pair.of(it.next(), ModelUtil.createFace(face, center, radius, uvs).lighti(15, 15)::render));
+
+			out.add(Pair.of(faceSideCenterRadius.condition, ModelUtil.createFace(face, center, radius, uvs).lighti(15, 15)::render));
 		}
-		assert !it.hasNext();
 
 		return out;
 	}
